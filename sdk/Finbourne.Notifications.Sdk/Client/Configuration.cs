60f6199f7a0434413fc0dea6ab3decc2a3830ca7
Concourse <concourse@finbourne.com>
2024-11-23 04:11:38 +0000

    notifications csharp updates for v2.2.5
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Notifications.Sdk/Client/Configuration.cs
@@ -599,7 +599,7 @@ namespace Finbourne.Notifications.Sdk.Client
             string report = "C# SDK (Finbourne.Notifications.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.1.1067\n";
+            report += "    Version of the API: 0.1.1069\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
